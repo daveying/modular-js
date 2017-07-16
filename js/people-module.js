750cc39abcdeda598f8aa4fe6435a1da70c550ad
daveying <daveying@163.com>
2017-07-16 21:53:38 +0800

    people module with object literal pattern is ok

--- js/people-module.js
+++ js/people-module.js
@@ -1,12 +1,44 @@
 var people = {
-	people: [],
-  init: function () {
-  	this.$debugLogDiv = $("#debugLogDiv");
-    this.$peopleList = $("#people-list");
-  },
-  outputHello: function (name) {
-  	this.$debugLogDiv.html("hello " + name + "!");
-  }
+    people: [],
+    init: function () {
+        this.$peopleList = $("#people-list");
+        this.$addButton = $("#add-button");
+        this.$textInput = $("#text-input");
+        this.bindEvents();
+    },
+    bindEvents: function () {
+        this.$addButton.on('click', this.addPerson.bind(this));
+        this.$peopleList.delegate('span.del', 'click', this.deletePerson.bind(this));
+        this.$peopleList.delegate('span.del', 'mouseover', this.changeColor.bind(this));
+        this.$peopleList.delegate('span.del', 'mouseout', this.changeColor.bind(this));
+    },
+    render: function () {
+        var lenPeople = this.people.length;
+        var htmlStr = "";
+        for (let i = 0; i < lenPeople; i++) {
+            htmlStr += `<li><span class="name">${this.people[i]}</span><span class="del">&times;</span></li>`
+        }
+        this.$peopleList.html(htmlStr);
+    },
+    addPerson: function () {
+        if (this.$textInput.val() === '') return;
+        this.people.push(this.$textInput.val());
+        this.render();
+        this.$textInput.val("");
+    },
+    deletePerson: function (event) {
+        var $remove = $(event.target).closest('li');
+        var i = this.$peopleList.find('li').index($remove);
+        this.people.splice(i, 1);
+        this.render();
+    },
+    changeColor: function (event) {
+        var $overred = $(event.target).closest('li');
+        if (event.type === "mouseover")
+            $overred.css("border-color", "red");
+        else
+            $overred.css("border-color", "grey");
+    }
 }
-
-people.outputHello("david");
+people.init();
+people.render();
